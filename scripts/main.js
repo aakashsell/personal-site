7ca1e10e4450b2b8352b0557da089ac3ead14ca7
aakashsell <aakashsell@gmail.com>
2021-10-29 13:22:01 -0400

    new site

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
+var lastScrollTop = 0;
 
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+      window.location.href = "#more-info"
+   } else {
+      this.window.location.href = "#start"
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
